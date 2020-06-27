8bda7ddf038226c3545f61e918eaf8d520aadcd2
vinhut <hutama.alvin@gmail.com>
2020-06-27 16:21:00 +0000

    test checkuser method

--- main_test.go
+++ main_test.go
@@ -6,10 +6,36 @@ import (
 	mocks_models "github.com/vinhut/posted/mocks_models"
 	mocks_services "github.com/vinhut/posted/mocks_services"
 
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+	"time"
 )
+
+func TestCheckUser(t *testing.T) {
+
+	now := time.Now()
+	token := "852a37a34b727c0e0b331806-7af4bdfdcc60990d427f383efecc8529289d040dd67e0753b9e2ee5a1e938402186f28324df23f6faa4e2bbf43f584ae228c55b00143866215d6e92805d470a1cc2a096dcca4d43527598122313be412e17fbefdcdab2fae02e06a405791d936862d4fba688b3c7fd784d4"
+	user_data := "{\"uid\": \"1\", \"email\": \"test@email.com\", \"role\": \"standard\", \"created\": \"" + now.Format("2006-01-02T15:04:05") + "\"}"
+
+	os.Setenv("KEY", "12345678901234567890123456789012")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mock_auth := mocks_services.NewMockAuthService(ctrl)
+	mock_auth.EXPECT().Check(gomock.Any(), gomock.Any()).Return(user_data, nil)
+
+	data, _ := checkUser(mock_auth, token)
+	test_data := &UserAuthData{}
+
+	if err := json.Unmarshal([]byte(user_data), test_data); err != nil {
+		fmt.Println(err)
+	}
+
+	assert.Equal(t, test_data, data)
+}
 
 func TestPing(t *testing.T) {
 
